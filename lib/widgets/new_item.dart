213847478165b71627feb3b7a6f38181d34f842c
Syed Shayan <3syedshayan@gmail.com>
2025-02-25 23:59:10 +0500

    Created a Basic Form, no Validation

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -16,7 +17,66 @@ class _NewItemState extends State<NewItem> {
       ),
       body: Padding(
         padding: EdgeInsets.all(12),
-        child: Text("Testing"),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Item Name",
+                ),
+                maxLength: 50,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Quantity",
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                SizedBox(width: 6),
+                                Text(category.value.name),
+                              ],
+                            ),
+                          )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("Reset"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Submit"),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
