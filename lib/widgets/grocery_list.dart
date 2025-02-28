0a5792d6c864f826c52b9c4664ccc74977167a87
Syed Shayan <3syedshayan@gmail.com>
2025-02-28 22:33:08 +0500

    Dismissible Widget to remove an item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -37,15 +37,23 @@ class _GroceryListState extends State<GroceryList> {
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 25,
-            height: 25,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            _groceryItems[index].quantity.toString(),
+        itemBuilder: (context, index) => Dismissible(
+          key: Key(_groceryItems[index].id),
+          onDismissed: (direction) {
+            setState(() {
+              _groceryItems.remove(_groceryItems[index]);
+            });
+          },
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 25,
+              height: 25,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
           ),
         ),
       );
