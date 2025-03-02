3fba5808d8ba22655e82f7706cd9f0a3ccba743e
Syed Shayan <3syedshayan@gmail.com>
2025-03-02 19:37:38 +0500

    Configured Firebase for posting and Getting grocery items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -12,23 +15,51 @@ class GroceryList extends StatefulWidget {
 List<GroceryItem> _groceryItems = [];
 
 class _GroceryListState extends State<GroceryList> {
+  void _loadItems() async {
+    final url = Uri.https(
+        'udemylearn-7cce0-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'grocery_items.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> extractedData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final data in extractedData.entries) {
+      final category = categories.entries
+          .firstWhere((e) => e.value.name == data.value['category'])
+          .value;
+      loadedItems.add(
+        GroceryItem(
+          id: data.key,
+          name: data.value['name'],
+          quantity: data.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {});
+    _groceryItems = loadedItems;
+    print(loadedItems);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
   void _onNewItem() async {
     var newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (context) => NewItem(),
       ),
     );
-    if (newItem == null) return;
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = Center(
       child: Text(
-        "No Items to Display \n Please Add an Item",
+        "No Items to Display, \n Please Add an Item.",
       ),
     );
 
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -15,18 +18,35 @@ class _NewItemState extends State<NewItem> {
   String _enteredName = "";
   var _selectedCategory = categories[Categories.vegetables]!;
   var _formKey = GlobalKey<FormState>();
-  void _submitForm() {
+  void _submitForm() async {
+    print("Executed");
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+
+      final url = Uri.https(
+          'udemylearn-7cce0-default-rtdb.asia-southeast1.firebasedatabase.app',
+          'grocery_items.json');
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.name,
+        }),
+      );
+      print("After Save");
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
-    Navigator.of(context).pop(
-      GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enteredQuantity,
-        category: _selectedCategory,
-      ),
-    );
   }
 
   @override
